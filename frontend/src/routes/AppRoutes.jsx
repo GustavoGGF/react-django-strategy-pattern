ef3cf005c2e0026fb5b3782ef73c87e60ba7991a
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-02-17 11:51:52 -0300

    feat(frontend): implement new application routes
    Added routing definitions for the new application modules.

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,14 +2,29 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Macapa from "../pages/Clients/Macapa/Macapa";
 import Varejao from "../pages/Clients/Varejao/Varejao";
+import { InputDataFileProvider } from "../context/InputDataFileProvider";
 
 const AppRoutes = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/clients/macapa" element={<Macapa />} />
-      <Route path="/clients/varejao" element={<Varejao />} />
+      <Route
+        path="/clients/macapa"
+        element={
+          <InputDataFileProvider>
+            <Macapa />
+          </InputDataFileProvider>
+        }
+      />
+      <Route
+        path="/clients/varejao"
+        element={
+          <InputDataFileProvider>
+            <Varejao />
+          </InputDataFileProvider>
+        }
+      />
     </Routes>
   </Router>
 );
