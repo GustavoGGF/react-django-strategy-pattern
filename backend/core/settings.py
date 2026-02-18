dfc0a8793aa5981703ce096c8744d4a064740be4
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-02-18 11:16:41 -0300

    feat(backend): configure MySQL for Docker environments
    Adjusted database settings and environment variables for containerized deployment.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -70,8 +70,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'macapa',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db-mysql', # Nome do serviço no Docker
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
