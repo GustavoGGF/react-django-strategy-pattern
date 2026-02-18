c4a4524abc41094788ff98862042c518d272a1f5
GustavoGGF <gustavoguilhermedefreitas@gmail.com>
2026-02-18 11:18:55 -0300

    feat(backend): fix schema and enhance header validation
    Strengthened HTTP_X_CLIENT_ID header validation for improved service routing.

--- backend/schema/schema.py
+++ backend/schema/schema.py
@@ -4,31 +4,41 @@ from services import MacapaService, VarejaoService
 # 1. formato exato que o seu JSON do Frontend possui
 class ContactInput(graphene.InputObjectType):
     name = graphene.String(required=True)
-    cellphone = graphene.String(required=True) # Nome exato da chave no seu JSON
-    email = graphene.String() # Opcional, aceita null se não vier no arquivo
+    cellphone = graphene.String(required=True) 
+    email = graphene.String() 
 
 # 2. Mutation vai retornar para o React
 class ImportContactsMutation(graphene.Mutation):
     class Arguments:
-        # Recebe a lista de contatos do JSON
         contacts = graphene.List(ContactInput, required=True)
+        # uma flag opcional para identificar se é manual
+        is_manual = graphene.Boolean(default_value=False)
 
     success = graphene.Boolean()
     message = graphene.String()
 
-    def mutate(self, info, contacts):
-        # identificador direto do header da requisição
-        client_id = info.context.headers.get('X-Client-Id')
-
-        if client_id == 'varejao':
-            service = VarejaoService() 
-        elif client_id == 'macapa':
-            service = MacapaService()
+    def mutate(self, info, contacts, is_manual):
+        print("Caiu no Schemma.py com os contatos: ", contacts)
+        # Seleção do Service (Macapá ou Varejão) via Header ou parâmetro
+        request = info.context
         
-        service.import_contacts(contacts)
-        return ImportContactsMutation(success=True)
+        client_id = request.META.get('HTTP_X_CLIENT_ID', 'macapa')
+        
+        service = MacapaService() if client_id == 'macapa' else VarejaoService()
 
-# 3. Unificam tudo no Schema global
+        try:
+            # O service processa a lista (seja com 1 ou 1000 itens)
+            service.import_contacts(contacts)
+            
+            origem = "Manual" if is_manual else "Arquivo"
+            return ImportContactsMutation(
+                success=True, 
+                message=f"Sucesso! {len(contacts)} contato(s) via {origem} inserido(s)."
+            )
+        except Exception as e:
+            return ImportContactsMutation(success=False, message=str(e))
+
+# 3. Unifica tudo no Schema global
 class Mutation(graphene.ObjectType):
     import_contacts = ImportContactsMutation.Field()
 
